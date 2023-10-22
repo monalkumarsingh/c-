6c9fc61defc204e06bf6d501cc2e17d2174f9503
Monal Singh <145989131+monalkumarsingh@users.noreply.github.com>
2023-10-22 18:51:47 +0530

    Add files via upload

---
+++ Basic c++2.cpp
@@ -0,0 +1,94 @@
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int n;
+//	cout<<"Enter the number";
+//	cin>>n;
+//	if(n>0)
+//	{
+//		cout<<"positive number:";
+//	}
+//	else
+//	{
+//		cout<<"absolute value:"<<-n;
+//	}
+//}
+
+// particular table print
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int i=19;
+//	for(i=19;i<=190;i=i+19)
+//	{
+//		cout<<"table is ="<<i<<endl;
+//	}
+//}
+
+// ap series genrated
+
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int n;
+//	cout<<"Enter the number:";
+//	cin>>n;
+//	int a=3;
+//	for(int i=1;i<=n;i++)
+//	{
+//		cout<<"The ap series is="<<a<<endl;
+//		a=a+3;
+//	}
+//}
+
+// Gp series genrated
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int n;
+//	cout<<"Enter the number";
+//	cin>>n;
+//	int a=3;
+//	for(int i=1;i<=10;i++)
+//	{
+//		cout<<"The Gp series is="<<a<<endl;
+//		a=a*2;
+//	}
+//}
+
+//highest factor is
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int n;
+//	cout<<"Enter the number";
+//	cin>>n;
+//	int f;
+//	for(int i=1;i<=n;i++)
+//	{
+//		if(n%i==0)
+//		f=i;
+//	}
+//	cout<<"the heihest factor is="<<f;
+//}
+
+#include<iostream>
+using namespace std;
+int main()
+{
+int n;
+	cout<<"Enter the number";
+	cin>>n;
+	int a=4;
+	int i;
+	for(i=1;i<=n;i++)
+	{
+		cout<<"The ap series is="<<a;
+		a=a+3;
+	}
+}
